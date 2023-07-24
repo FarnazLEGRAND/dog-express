c664f90cb8a66db5655b88739181484f84761c49
FarnazLEGRAND <116880622+FarnazLEGRAND@users.noreply.github.com>
2023-07-24 13:53:02 +0200

    api done

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,17 @@
-import 'dotenv/config'; //Import nécessaire pour que les fichier .env soient pris en compte et charger dans le process.env
-import './auth-config';
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
+import { dogController } from './controller/dog-controller';
 
-
-
-//On choisit le port sur lequel tournera l'application en indiquant 3000 par défaut mais modifiable par une variable d'environnement
 const port = process.env.PORT || 8000;
 
-//On crée l'application express
 const app = express();
-//On indique à notre application de convertir les body des requêtes JSON en objet js
 app.use(express.json());
 app.use(cors());
 
-//Ici, on assigne notre contrôleur à la route /api/example, ce qui fait que toutes les routes définies dans le contrôleur seront préfixées par /api/example
+app.use('/api/dog', dogController);
 
 
-
-//On dit à l'application d'écouter les requêtes http sur le port choisit
 app.listen(port, () => {
-    console.log('listening on http://localhost:'+port);
-});
+    console.log('Server is listening on http://localhost:'+port);
+})
